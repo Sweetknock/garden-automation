92d2b5a94c875e72edddcc24b3d3eb1d5f8ff50f
Sweetknock <JosephADodson@hotmail.com>
2023-05-29 23:22:52 -0700

    Data to csv files by date and append each run

--- get_sensor_info.py
+++ get_sensor_info.py
@@ -1,9 +1,53 @@
 #!/usr/bin/env python3
+from os import path
 from serial import Serial
+from datetime import datetime
+import pandas as pd
+
+class UpdateDataFile:
+    def update(ser):
+        
+        #Get current date and time
+        now = datetime.now()
+        time_string = now.strftime("%H%M")
+        date_string = now.strftime("%Y_%m_%d")
+
+        #Put time and date in column data
+        data_dict = {}
+        data_dict["Time"] = time_string
+        data_dict["Date"] = date_string
+        
+        #Also use date for file name
+        filename = "{}_daily_data.csv".format(date_string) 
+        if path.exists(filename):
+            df = pd.read_csv(filename)
+        else:
+            df = pd.DataFrame()
+
+
+        #Loop average# times and average at the end
+        averages = 3
+        for i in range(averages):
+            #Initialize data variables and loop until filled
+            humidity_data = []
+            temperature_data = []
+            while len(humidity_data)<(i+1) or len(temperature_data)<(i+1):
+                line = ser.readline().decode('utf-8').rstrip()
+                if line.startswith("Humidity (%): "):  humidity_data.append(float(line.replace("Humidity (%): ", "")))
+                elif line.startswith("Temperature  (F): "): temperature_data.append(float(line.replace("Temperature  (F): ", "")))
+
+        data_dict["Humidity(%)"] = round(sum(humidity_data)/averages,1)
+        data_dict["Temperature(F)"] = round(sum(temperature_data)/averages,1)
+        df = pd.concat([df, pd.DataFrame(data_dict,index=[i])])
+
+        df.to_csv(filename, index=False)
+        return df
+
 if __name__ == '__main__':
-    ser = Serial('COM4', 9600, timeout=1)
+    ser = Serial('COM4', 9600, timeout=1) # For Windows Environment
+    #ser = Serial('/dev/ttyACM0', 9600, timeout=1)# For linux environment
     ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+
+    df = UpdateDataFile.update(ser)
+
+
