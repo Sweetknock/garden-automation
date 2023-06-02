3aa1f89bc9968e40da9b3b7aaaea19db9cb5348e
Sweetknock <JosephADodson@hotmail.com>
2023-06-02 13:27:16 -0700

    Sensor code changes.

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
-from get_sensor_info import UpdateDataFile, Serial
+from get_sensor_info import UpdateDataFile, Serial, name, sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=("POST", "GET"))
 def html_table():
-    #ser = Serial('COM4', 9600, timeout=1) # For Windows Environment
-    ser = Serial('/dev/ttyACM0', 9600, timeout=1)# For linux environment
+    if name == 'nt':
+        ser = Serial('COM4', 9600, timeout=1)# For linux environment
+    elif name == 'posix':
+        ser = Serial('/dev/ttyACM0', 9600, timeout=1)# For linux environment
+    else:
+        print("System not supported.")
+        sys.exit()
+        
     ser.reset_input_buffer()
     df = UpdateDataFile.update(ser)
     return render_template('index.html', tables=[df.to_html(classes='table-bordered', index=False)], titles=["Bed Data"])
--- get_sensor_info.py
+++ get_sensor_info.py
@@ -1,26 +1,27 @@
-from os import path, system
+#!/usr/bin/env python3
+from os import path, name
+import sys
 from serial import Serial
 from datetime import datetime
 import pandas as pd
 
 class UpdateDataFile:
-    def update(ser):
-        
-        #Get current date and time
-        now = datetime.now()
-        time_string = now.strftime("%H%M")
-        date_string = now.strftime("%Y_%m_%d")
+    def update(self, ser):
 
+        date_string, time_string =  self.get_datetime()
+        filename = "{}_daily_data.csv".format(date_string) 
+
+        #initialize dictionary
         #Put time and date in column data
         data_dict = {}
         data_dict["Time"] = time_string
         data_dict["Date"] = date_string
-        
+
         #Also use date for file name
-        filename = "/var/www/html/garden-automation/{}_daily_data.csv".format(date_string) 
+        #filename = "/var/www/html/garden-automation/{}_daily_data.csv".format(date_string) 
         if path.exists(filename):
-            df = pd.read_csv(filename)
-            system("sudo chown -R www-data /var/www/html/garden-automation && sudo chmod -R 774 /var/www/html/garden-automation && sudo chgrp -R www-data /var/www/html/garden-automation".format(filename,filename,filename))
+            df = self.get_database(filename)
+            #system("sudo chown -R www-data /var/www/html/garden-automation && sudo chmod -R 774 /var/www/html/garden-automation && sudo chgrp -R www-data /var/www/html/garden-automation".format(filename,filename,filename))
         else:
             df = pd.DataFrame()
 
@@ -32,9 +33,19 @@ class UpdateDataFile:
             humidity_data = []
             temperature_data = []
             while len(humidity_data)<(i+1) or len(temperature_data)<(i+1):
-                line = ser.readline().decode('utf-8').rstrip()
-                if line.startswith("Humidity (%): "):  humidity_data.append(float(line.replace("Humidity (%): ", "")))
-                elif line.startswith("Temperature  (F): "): temperature_data.append(float(line.replace("Temperature  (F): ", "")))
+                line1 = ser.readline().decode('utf-8').rstrip()
+                if line1 == '': line1 = ser.readline().decode('utf-8').rstrip()
+                line2 = ser.readline().decode('utf-8').rstrip()
+                
+                if line1.startswith("Humidity(%),"):  
+                    humidity_data.append(float(line1.replace("Humidity(%),", "")))
+                    temperature_data.append(float(line2.replace("Temperature(F),", "")))
+                elif line1.startswith("Temperature(F),"):
+                    humidity_data.append(float(line2.replace("Humidity(%),", ""))) 
+                    temperature_data.append(float(line1.replace("Temperature(F),", "")))
+                else:
+                    print("Data read error.")
+                    continue
 
         data_dict["Humidity(%)"] = round(sum(humidity_data)/averages,1)
         data_dict["Temperature(F)"] = round(sum(temperature_data)/averages,1)
@@ -42,12 +53,30 @@ class UpdateDataFile:
 
         df.to_csv(filename, index=False)
         return df
+    
+    def get_datetime(self):
+        #Get current date and time
+        now = datetime.now()
+        time_string = now.strftime("%H%M")
+        date_string = now.strftime("%Y_%m_%d")
+
+        return date_string, time_string
+
+    def get_database(self, filename):
+        if path.exists(filename):
+            df = pd.read_csv(filename)
+            return df
 
 if __name__ == '__main__':
-    #ser = Serial('COM4', 9600, timeout=1) # For Windows Environment
-    ser = Serial('/dev/ttyACM0', 9600, timeout=1)# For linux environment
+    if name == 'nt':
+        ser = Serial('COM4', 9600, timeout=1)# For linux environment
+    elif name == 'posix':
+        ser = Serial('/dev/ttyACM0', 9600, timeout=1)# For linux environment
+    else:
+        print("System not supported.")
+        sys.exit()
     ser.reset_input_buffer()
 
-    df = UpdateDataFile.update(ser)
+    df = UpdateDataFile().update(ser)
 
 
